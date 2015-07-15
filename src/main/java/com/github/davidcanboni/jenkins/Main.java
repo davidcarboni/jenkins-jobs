d0b539d3ae1adc1020bd9a41267c07c3cb2476e1
David Carboni <david@carboni.io>
2015-07-15 17:01:15 +0100

    Placeholder class with a comment about how to provide the username and password.

---
+++ src/main/java/com/github/davidcanboni/jenkins/Main.java
@@ -0,0 +1,13 @@
+package com.github.davidcanboni.jenkins;
+
+/**
+ * Updates all Jenkins jobs.
+ *
+ * To get this class to run, you'll need to pass the username and password as properties on the command line.
+ *
+ * If you're using intellij, you'll need to edit the run configuration and add something like the following to the VM options:
+ *
+ * -Dusername=user -Dpassword=pass
+ */
+public class Main {
+}
