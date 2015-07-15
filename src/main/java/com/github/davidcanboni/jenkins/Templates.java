946cf30e21c4e517909d68fd39978da99fcb206c
David Carboni <david@carboni.io>
2015-07-15 17:40:58 +0100

    Fixed template path constants.

--- src/main/java/com/github/davidcanboni/jenkins/Templates.java
+++ src/main/java/com/github/davidcanboni/jenkins/Templates.java
@@ -6,9 +6,9 @@ package com.github.davidcanboni.jenkins;
 public class Templates {
 
     static String templateFolder = "/templates/";
-    static String configDeploy = templateFolder + "templates/config-deploy.xml";
-    static String configDocker = templateFolder + "templates/config-docker.xml";
-    static String configLibrary = templateFolder + "templates/config-library.xml";
-    static String configMaven = templateFolder + "templates/config-maven.xml";
-    static String configMonitor = templateFolder + "templates/config-monitor.xml";
+    static String configDeploy = templateFolder + "config-deploy.xml";
+    static String configDocker = templateFolder + "config-docker.xml";
+    static String configLibrary = templateFolder + "config-library.xml";
+    static String configMaven = templateFolder + "config-maven.xml";
+    static String configMonitor = templateFolder + "config-monitor.xml";
 }
