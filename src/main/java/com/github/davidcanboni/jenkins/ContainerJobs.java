467b96a37a4b7d339cee68e88f76e108bcd1010f
David Carboni <david@carboni.io>
2015-07-16 17:48:52 +0100

    Added upstream and downstream jobs, plus container build steps.

--- src/main/java/com/github/davidcanboni/jenkins/ContainerJobs.java
+++ src/main/java/com/github/davidcanboni/jenkins/ContainerJobs.java
@@ -1,10 +1,13 @@
 package com.github.davidcanboni.jenkins;
 
+import com.github.davidcanboni.jenkins.values.Environment;
+import com.github.davidcanboni.jenkins.values.GitRepo;
 import com.github.davidcanboni.jenkins.xml.Xml;
 import com.github.davidcarboni.ResourceUtils;
 import com.github.onsdigital.http.Endpoint;
 import com.github.onsdigital.http.Http;
 import com.github.onsdigital.http.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.w3c.dom.Document;
 
@@ -23,10 +26,14 @@ public class ContainerJobs {
         return document;
     }
 
-    public static Document forRepo(URL gitUrl, Environment environment) throws IOException {
+    public static Document forRepo(GitRepo gitRepo, Environment environment) throws IOException {
         Document document = getTemplate();
-        setGitUrl(gitUrl, document);
+        setGitUrl(gitRepo.url, document);
         setBranch(environment.name(), document);
+        setUpstreamMavenBulid(gitRepo, environment, document);
+        setDownstreamDeployJobs(environment, document);
+        createImageCommand(gitRepo, environment, document);
+        pushImageCommand(gitRepo, environment, document);
         return document;
     }
 
@@ -42,6 +49,33 @@ public class ContainerJobs {
         Xml.setTextValue(template, "//hudson.plugins.git.BranchSpec/name", "*/" + branch);
     }
 
+    private  static void setUpstreamMavenBulid(GitRepo gitRepo, Environment environment, Document template) {
+        Xml.setTextValue(template, "//jenkins.triggers.ReverseBuildTrigger/upstreamProjects",  MavenJobs.jobName(gitRepo, environment));
+    }
+
+    private static void setDownstreamDeployJobs(Environment environment, Document template) throws IOException {
+        String[] jobNames = new String[]{DeployJobs.jobNameWebsite(environment),DeployJobs.jobNamePublishing(environment)};
+        String childProjects = StringUtils.join(jobNames, ", ");
+        Xml.setTextValue(template, "//publishers/hudson.tasks.BuildTrigger/childProjects", childProjects);
+    }
+    //Deploy publishing (develop), Deploy website (develop)
+
+    private static void createImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = gitRepo.name();
+        String tag = environment.name();
+        String imageTag = registry+"/"+image+":"+tag;
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.CreateImageCommand']/imageTag", imageTag);
+    }
+
+    private static void pushImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = registry+"/"+gitRepo.name();
+        String tag = environment.name();
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.PushImageCommand']/image", image);
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.PushImageCommand']/tag", tag);
+    }
+
 
     public static void create(GitRepo gitRepo, Environment environment) throws IOException, URISyntaxException {
 
@@ -49,20 +83,20 @@ public class ContainerJobs {
 
             http.addHeader("Content-Type", "application/xml");
             String jobName = jobName(gitRepo, environment);
-            Document config = forRepo(gitRepo.url, environment);
+            Document config = forRepo(gitRepo, environment);
 
             if (!Jobs.exists(jobName)) {
 
                 System.out.println("Creating Maven job " + jobName);
 
                 // Set the URL and create:
-                //create(jobName, config, http);
+                create(jobName, config, http);
 
             } else {
 
                 System.out.println("Updating Maven job " + jobName);
                 Endpoint endpoint = new Endpoint(Jobs.jenkins, "/job/" + jobName + "/config.xml");
-                //update(jobName, config, http, endpoint);
+                update(jobName, config, http, endpoint);
 
             }
 
@@ -78,20 +112,20 @@ public class ContainerJobs {
 
         // Post the config XML to create the job
         Endpoint endpoint = Jobs.createItem.setParameter("name", jobName);
-        Response<String> create = http.post(endpoint, config, String.class);
-        if (create.statusLine.getStatusCode() != 200) {
-            System.out.println(create.body);
-            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + create.statusLine.getReasonPhrase());
+        Response<String> response = http.post(endpoint, config, String.class);
+        if (response.statusLine.getStatusCode() != 200) {
+            System.out.println(response.body);
+            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + response.statusLine.getReasonPhrase());
         }
     }
 
     private static void update(String jobName, Document config, Http http, Endpoint endpoint) throws IOException {
 
         // Post the config XML to update the job
-        Response<String> create = http.post(endpoint, config, String.class);
-        if (create.statusLine.getStatusCode() != 200) {
-            System.out.println(create.body);
-            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + create.statusLine.getReasonPhrase());
+        Response<String> response = http.post(endpoint, config, String.class);
+        if (response.statusLine.getStatusCode() != 200) {
+            System.out.println(response.body);
+            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + response.statusLine.getReasonPhrase());
         }
     }
 
