062680b6db2cb1227580133f9062e140d6c706f5
David Carboni <david@carboni.io>
2015-07-21 17:00:16 +0100

    Updated to match ContainerJobs rather than MavenJobs.

--- src/main/java/com/github/davidcanboni/jenkins/MavenNodeJobs.java
+++ src/main/java/com/github/davidcanboni/jenkins/MavenNodeJobs.java
@@ -2,12 +2,12 @@ package com.github.davidcanboni.jenkins;
 
 import com.github.davidcanboni.jenkins.values.Environment;
 import com.github.davidcanboni.jenkins.values.GitRepo;
-import com.github.davidcanboni.jenkins.values.JobCategory;
 import com.github.davidcanboni.jenkins.xml.Xml;
 import com.github.davidcarboni.ResourceUtils;
 import com.github.onsdigital.http.Endpoint;
 import com.github.onsdigital.http.Http;
 import com.github.onsdigital.http.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -21,12 +21,22 @@ import java.net.URL;
  */
 public class MavenNodeJobs {
 
+    public static Document forRepo(URL gitUrl) throws IOException {
+        Document document = getTemplate();
+        setGitUrl(gitUrl, document);
+        return document;
+    }
+
     public static Document forRepo(GitRepo gitRepo, Environment environment) throws IOException {
         Document document = getTemplate();
         setGitUrl(gitRepo.url, document);
         setBranch(environment.name(), document);
-        setContainerJob(gitRepo, environment, document);
+        setDownstreamDeployJobs(environment, document);
         addNodeBuildStep(document);
+        removeImageCommand(gitRepo, environment, document);
+        tagImageCommand(gitRepo, environment, document);
+        createImageCommand(gitRepo, environment, document);
+        pushImageCommand(gitRepo, environment, document);
         return document;
     }
 
@@ -42,8 +52,45 @@ public class MavenNodeJobs {
         Xml.setTextValue(template, "//hudson.plugins.git.BranchSpec/name", "*/" + branch);
     }
 
-    private static void setContainerJob(GitRepo gitRepo, Environment environment, Document template) throws IOException {
-        Xml.setTextValue(template, "//publishers/hudson.tasks.BuildTrigger/childProjects", ContainerJobs.jobName(gitRepo, environment));
+    private static void setDownstreamDeployJobs(Environment environment, Document template) throws IOException {
+        String[] jobNames = new String[]{DeployJobs.jobNameWebsite(environment), DeployJobs.jobNamePublishing(environment)};
+        String childProjects = StringUtils.join(jobNames, ", ");
+        Xml.setTextValue(template, "//publishers/hudson.tasks.BuildTrigger/childProjects", childProjects);
+    }
+    //Deploy publishing (develop), Deploy website (develop)
+
+    private static void removeImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = gitRepo.name();
+        String tag = environment.name();
+        String imageTag = registry + "/" + image + ":" + tag + "_previous";
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.RemoveImageCommand']/imageName", imageTag);
+    }
+
+    private static void tagImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = gitRepo.name();
+        String tag = environment.name() + "_previous";
+        String imageName = registry + "/" + image;
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.TagImageCommand']/image", imageName);
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.TagImageCommand']/repository", imageName);
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.TagImageCommand']/tag", tag);
+    }
+
+    private static void createImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = gitRepo.name();
+        String tag = environment.name();
+        String imageTag = registry + "/" + image + ":" + tag;
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.CreateImageCommand']/imageTag", imageTag);
+    }
+
+    private static void pushImageCommand(GitRepo gitRepo, Environment environment, Document template) throws IOException {
+        String registry = Environment.registry().getHost();
+        String image = registry + "/" + gitRepo.name();
+        String tag = environment.name();
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.PushImageCommand']/image", image);
+        Xml.setTextValue(template, "//dockerCmd[@class='org.jenkinsci.plugins.dockerbuildstep.cmd.PushImageCommand']/tag", tag);
     }
 
     private static void addNodeBuildStep(Document template) {
@@ -74,24 +121,21 @@ public class MavenNodeJobs {
             if (!Jobs.exists(jobName)) {
 
                 System.out.println("Creating Maven job " + jobName);
-
-                // Set the URL and create:
-                //create(jobName, config, http);
+                create(jobName, config, http);
 
             } else {
 
                 System.out.println("Updating Maven job " + jobName);
                 Endpoint endpoint = new Endpoint(Jobs.jenkins, "/job/" + jobName + "/config.xml");
-                //update(jobName, config, http, endpoint);
+                update(jobName, config, http, endpoint);
 
             }
-            System.out.println(Xml.toString(config));
 
         }
     }
 
     public static String jobName(GitRepo gitRepo, Environment environment) {
-        return JobCategory.Maven + " " + WordUtils.capitalize(environment.name()) + " " + WordUtils.capitalize(gitRepo.name());
+        return WordUtils.capitalize(gitRepo.name()) + " container (" + environment.name() + ")";
     }
 
     private static void create(String jobName, Document config, Http http) throws IOException {
@@ -99,20 +143,20 @@ public class MavenNodeJobs {
 
         // Post the config XML to create the job
         Endpoint endpoint = Jobs.createItem.setParameter("name", jobName);
-        Response<String> create = http.post(endpoint, config, String.class);
-        if (create.statusLine.getStatusCode() != 200) {
-            System.out.println(create.body);
-            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + create.statusLine.getReasonPhrase());
+        Response<String> response = http.post(endpoint, config, String.class);
+        if (response.statusLine.getStatusCode() != 200) {
+            System.out.println(response.body);
+            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + response.statusLine.getReasonPhrase());
         }
     }
 
     private static void update(String jobName, Document config, Http http, Endpoint endpoint) throws IOException {
 
         // Post the config XML to update the job
-        Response<String> create = http.post(endpoint, config, String.class);
-        if (create.statusLine.getStatusCode() != 200) {
-            System.out.println(create.body);
-            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + create.statusLine.getReasonPhrase());
+        Response<String> response = http.post(endpoint, config, String.class);
+        if (response.statusLine.getStatusCode() != 200) {
+            System.out.println(response.body);
+            throw new RuntimeException("Error setting configuration for job " + jobName + ": " + response.statusLine.getReasonPhrase());
         }
     }
 
